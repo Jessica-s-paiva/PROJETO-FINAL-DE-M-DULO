82ca1771fb95c9f891e8b550b11e09c42d5bf981
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-04-24 02:25:30 -0300

    troca de cores do banner com clique

--- js/servicos/main.js
+++ js/servicos/main.js
@@ -1,5 +1,24 @@
 $("#cor1").click((event) => {
     event.preventDefault();
-    console.log('oi');
-    $("#banner").css("background", "#DAA520");
-  });
+    $("#banner").css("background", "#FFF900");
+});
+
+$("#cor2").click((event) => {
+    event.preventDefault();
+    $("#banner").css("background", "#8EFF00");
+});
+
+$("#cor3").click((event) => {
+    event.preventDefault();
+    $("#banner").css("background", "#F186B4");
+});
+
+$("#cor4").click((event) => {
+    event.preventDefault();
+    $("#banner").css("background", "#86C0F1");
+});
+
+$("#cor5").click((event) => {
+    event.preventDefault();
+    $("#banner").css("background", "#FFA500");
+});
