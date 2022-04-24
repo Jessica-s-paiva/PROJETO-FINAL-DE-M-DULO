30e9172ce0c9bda02dd7305e2fa5cee854f92a22
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-04-24 03:23:35 -0300

    criando menu do template

--- js/servicos/main.js
+++ js/servicos/main.js
@@ -1,24 +1,24 @@
 $("#cor1").click((event) => {
     event.preventDefault();
-    $("#banner").css("background", "#FFF900");
+    $(".banner").css("background", "#FFF900");
 });
 
 $("#cor2").click((event) => {
     event.preventDefault();
-    $("#banner").css("background", "#8EFF00");
+    $(".banner").css("background", "#8EFF00");
 });
 
 $("#cor3").click((event) => {
     event.preventDefault();
-    $("#banner").css("background", "#F186B4");
+    $(".banner").css("background", "#F186B4");
 });
 
 $("#cor4").click((event) => {
     event.preventDefault();
-    $("#banner").css("background", "#86C0F1");
+    $(".banner").css("background", "#86C0F1");
 });
 
 $("#cor5").click((event) => {
     event.preventDefault();
-    $("#banner").css("background", "#FFA500");
+    $(".banner").css("background", "#FFA500");
 });
