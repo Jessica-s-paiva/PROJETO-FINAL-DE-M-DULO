ef1b554795fc53afc32e24e92f18d54b4469ec69
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-04-24 01:23:15 -0300

    alterei fundo do template com DOM

--- js/servicos/main.js
+++ js/servicos/main.js
@@ -1,5 +1,5 @@
 $("#cor1").click((event) => {
     event.preventDefault();
-    controller.criaLista();
-    console.log(controller.ListaDeNotas);
-  })
+    console.log('oi');
+    $("#banner").css("background", "#DAA520");
+  });
--- js/servicos/model/model.js
+++
@@ -1,5 +0,0 @@
-class Cores{
-    constructor(){
-        
-    }
-}
